f5893ca3951c47087cea139421a537381ccbf059
33opo <opost33@gmail.com>
2022-08-18 11:23:26 -0700

    test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./App";
+import App from "./App";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="" element={<Home />}/>
-        <Route path="/Home" element={<Home />}/>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
