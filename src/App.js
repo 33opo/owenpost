91ee4d7ba61b12d2c0cd4efcd3fb4d216d371970
Owen Post <opost33@gmail.com>
2022-08-24 00:21:04 -0700

    onMouseEnter

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import React from "react";
 //width="6130" height="3125"
 //<img className="back" src={require('./Styles/sizedDesk.jpg')} alt="Workplace" usemap="#workmap" width="100%" height="100%"/> 
 function App() {
+
+  const [isShown, setIsShown] = React.useState(false);
+  
   return (
 <div className="back">
 <img src={require('./Styles/sizedDesk.jpg')} alt="Workplace" usemap="#workmap"/>
@@ -13,10 +16,14 @@ function App() {
   </div>
   <div className="Text">
   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
+  {isShown && (
+        <div>
+          I'll appear when you hover over the button.
+        </div>
+      )}
   </div>
 <map name="workmap">
-  <area target="_top" alt="Coffee" title="Coffee" href="coffee.html" coords="6200,950,350" shape="circle"/>
+  <area onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)} target="_top" alt="Coffee" title="Coffee" href="coffee.html" coords="6200,950,350" shape="circle"/>
 </map>
 </div>
 
