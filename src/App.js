82d0a43899f112b353434be20de81b69763515eb
Owen Post <opost33@gmail.com>
2022-08-24 14:23:53 -0700

    added notable stuff

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import './Styles/App.css';
 import React from "react";
-//import Projects from "./Components/projects.json"
 
-//width="6130" height="3125"
-//<img className="back" src={require('./Styles/sizedDesk.jpg')} alt="Workplace" usemap="#workmap" width="100%" height="100%"/> 
-
-//<img src={require('./Styles/sizedDesk.jpg')} alt="Workplace" usemap="#workmap"/>
-// <map name="workmap">
-//  <area onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)} target="_top" alt="Coffee" title="Coffee" href="coffee.html" coords="6200,950,350" shape="circle"/>
-//  </map>
-//
-//
 function App() {
-
-  const [isShown, setIsShown] = React.useState(false);
-
   window.onload = function () {
     var ImageMap = function (map, img) {
             var n,
                 areas = map.getElementsByTagName('area'),
                 len = areas.length,
                 coords = [],
-                previousWidth = 1820;
+                previousWidth = 1890;
+                //1890 for smaller screen
+                //
             for (n = 0; n < len; n++) {
                 coords[n] = areas[n].coords.split(',');
             }
@@ -44,86 +33,87 @@ function App() {
     imageMap.resize();
     return;
 }
+const [mouse, setMouse] = React.useState(false);
+const [coffee, setCoffee] = React.useState(false);
+const [start, setStart] = React.useState(true);
+
+const mouseTrue = () => {
+  setMouse(true);
+  setCoffee(false);
+  setStart(false);
+}
+
+const coffeeTrue = () => {
+  setCoffee(true);
+  setMouse(false);
+  setStart(false);
+}
 
   return (
   <div className="back">
 
     <img id="img_ID" src={require('./Styles/sizedDesk.jpg')} alt="Workplace" usemap="#map"/>
     <map id="map_ID" name="map">
-        <area
-          alt="headphones"
-          title="headphones"
-          href="#headhphones"
-          coords="120,364,200"
-          shape="circle"
+        <area 
+          alt="mouse" 
+          onClick={() => mouseTrue()}
+          title="mouse" 
+          href="#mouse" 
+          coords="1016,50,1041,53,1063,58,1083,73,1083,96,1080,113,1075,140,1070,163,1061,183,1048,195,1031,198,1015,200,991,196,969,191,956,178,953,156,961,128,968,98,976,66,993,53" 
+          shape="poly"
         />
-        <area
-          alt="peripherals"
-          title="peripherals"
-          href="#peripherals"
-          coords="260,314,737,478"
-          shape="rect"
-        />
-        <area
-          alt="stationery"
-          title="stationery"
-          href="#stationery"
-          coords="526,296,735,296,729,16,563,16,563,108,524,108"
+        <area 
+          alt="coffee"
+          onClick={() => coffeeTrue()}
+          title="coffee" 
+          href="#coffee" 
+          coords="1544,159,1577,164,1604,172,1627,187,1644,202,1656,224,1657,249,1651,274,1641,292,1627,309,1606,319,1584,327,1559,332,1527,331,1504,326,1480,319,1457,304,1437,279,1429,249,1435,219,1450,195,1475,175,1504,162" 
           shape="poly"
         />
       </map>
-      <div className="popup" id="headhphones">
-        An awesome pair of headphones!
-      </div>
-      <div className="popup" id="peripherals">
-        These are the peripherals: the keyboard and the mouse.
-      </div>
-      <div className="popup" id="stationery">
-        This is my stationery: a notepad and a pen.
-      </div>
+      {start && (
+        <div>
+        <div className="Name">
+        Owen Post
+        </div>
+        <div className="Text">
+          Welcome to my desk! Feel free to look around as its a little messy around here.
+        </div>
+        </div>
+      )}
 
+      {coffee && (
+        <div className="Text">
+          Coffee
+        </div>
+      )}
+
+      {mouse && (
+        <div>
+        <div className="Notable">
+          Notable
+          <div> 
+          This application that allows a user to upload a youtube video or pdf and take notes.
+          Users can also make presentations and use many more features as shown in the video!
+          </div>
+        </div>
+        <div className="video-responsive">
+    <iframe
+      width="853"
+      height="480"
+      src={`https://www.youtube.com/embed/NVWfohvp_Lw`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div>
+      </div>
+      )}
 
   </div>
   );
 }
-
-/*
-    <img src={require('./Styles/Desk.jpg')} alt="Workplace" usemap="#workmap"/>
-      <map name="workmap">
-        <area
-          alt="headphones"
-          title="headphones"
-          href="#headhphones"
-          coords="120,364,200"
-          shape="circle"
-        />
-        <area
-          alt="peripherals"
-          title="peripherals"
-          href="#peripherals"
-          coords="260,314,737,478"
-          shape="rect"
-        />
-        <area
-          alt="stationery"
-          title="stationery"
-          href="#stationery"
-          coords="526,296,735,296,729,16,563,16,563,108,524,108"
-          shape="poly"
-        />
-      </map>
-      <div className="popup" id="headhphones">
-        An awesome pair of headphones!
-      </div>
-      <div className="popup" id="peripherals">
-        These are the peripherals: the keyboard and the mouse.
-      </div>
-      <div className="popup" id="stationery">
-        This is my stationery: a notepad and a pen.
-      </div>
-*/
-
-
 
 /*
   <div className="Name">
